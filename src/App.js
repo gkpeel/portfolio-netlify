153afce0a655e9291da2076205cbb29e1a5ee3e2
gkpeel <gkpeel@gmail.com>
2020-06-13 14:39:16 -0400

    added oist

--- src/App.js
+++ src/App.js
@@ -1,82 +1,81 @@
 // import React
-import React, { Component } from "react"
+import React, { Component } from 'react';
 
 // import React Transition Group
-import { TransitionGroup, CSSTransition } from "react-transition-group"
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 // import React Router
-import { Switch, Route } from "react-router-dom"
+import { Switch, Route } from 'react-router-dom';
 
 // import Bootstrap, Styled Components, Fonts, and CSS Transitions
-import { Container } from "react-bootstrap"
-import styled from "styled-components"
-import "./App.css"
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+import './App.css';
 
 // import FontAwesome Library/build Library
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faAngleDoubleRight, faCode, faEye, faUserCircle, faFileDownload } from '@fortawesome/free-solid-svg-icons'
-import { fab } from '@fortawesome/free-brands-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngleDoubleRight, faCode, faEye, faUserCircle, faFileDownload } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 // import Navigation and Pages
-import Navigation from "./components/Navigation"
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Projects from "./pages/Projects"
-import Resume from "./pages/Resume"
-import Blog from "./pages/Blog"
-import Contact from "./pages/Contact"
-import NoMatch from "./pages/NoMatch"
-import "./App.css"
+import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Resume from './pages/Resume';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import OnlyInSelectTheaters from './pages/OnlyInSelectTheaters';
+import NoMatch from './pages/NoMatch';
+import './App.css';
 
-library.add(faAngleDoubleRight, faCode, fab, faEye, faUserCircle, faFileDownload)
+library.add(faAngleDoubleRight, faCode, fab, faEye, faUserCircle, faFileDownload);
 
 const Page = styled.div`
-  position: absolute;
-  top: 138px;
-  left: 0;
-  right: 0;
-  color: #FFFFFF;
-`
+	position: absolute;
+	top: 138px;
+	left: 0;
+	right: 0;
+	color: #ffffff;
+`;
 const PortfolioContainer = styled(Container)`
-  max-width: 1920px;
-  @media(min-width:576px) {
-    padding-left: 32px;
-    padding-right: 32px;
-  }
-`
+	max-width: 1920px;
+	@media (min-width: 576px) {
+		padding-left: 32px;
+		padding-right: 32px;
+	}
+`;
 
 class App extends Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Navigation />
-        <Route render={({ location }) => (
-          <TransitionGroup>
-            <CSSTransition
-              key={location.key}
-              timeout={450}
-              classNames="fade"
-            >
-              <Page>
-                <PortfolioContainer fluid>
-                  <Switch location={location}>
-                    <Route exact path="/" component={Home} />
-                    <Route path="/about" component={About} />
-                    <Route path="/projects" component={Projects} />
-                    <Route path="/resume" component={Resume} />
-                    <Route path="/blog" component={Blog} />
-                    <Route path="/contact" component={Contact} />
-                    <Route component={NoMatch} />
-                  </Switch>
-                </PortfolioContainer>
-              </Page>
-            </CSSTransition>
-          </TransitionGroup>
-        )} />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Navigation />
+				<Route
+					render={({ location }) => (
+						<TransitionGroup>
+							<CSSTransition key={location.key} timeout={450} classNames="fade">
+								<Page>
+									<PortfolioContainer fluid>
+										<Switch location={location}>
+											<Route exact path="/" component={Home} />
+											<Route path="/about" component={About} />
+											<Route path="/projects" component={Projects} />
+											<Route path="/resume" component={Resume} />
+											<Route path="/blog" component={Blog} />
+											<Route path="/contact" component={Contact} />
+											<Route path="/oist" component={OnlyInSelectTheaters} />
+											<Route component={NoMatch} />
+										</Switch>
+									</PortfolioContainer>
+								</Page>
+							</CSSTransition>
+						</TransitionGroup>
+					)}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ src/pages/OnlyInSelectTheaters/index.js
@@ -0,0 +1,92 @@
+import React, { Component } from 'react';
+import { Row, Col, Button } from 'react-bootstrap';
+import styled from 'styled-components';
+import Section from '../../components/Section';
+import ContentHeader from '../../components/ContentHeader';
+import Script from '../../pdf/only-in-select-theaters.pdf';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const DownloadButton = styled(Button)`
+	background-color: #ff00ff;
+	display: block;
+	width: 100%;
+	margin-bottom: 2.5rem;
+	margin-top: 1rem;
+	border-color: #ff00ff;
+	border: 1px solid transparent;
+	border-radius: 0.25rem;
+	padding: 0.5rem 1rem;
+	text-align: center;
+	color: rgb(20, 20, 25) !important;
+	font-family: 'Permanent Marker', sans-serif;
+	font-size: 1.25rem;
+	font-weight: 400;
+	text-decoration: none;
+	vertical-align: middle;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+	line-height: 1.5;
+	cursor: pointer;
+	transition: color 0.15s ease-in-out, background-color 300ms ease-in-out, border-color 300ms ease-in-out, box-shadow 300ms ease-in-out;
+
+	& .cta-icon {
+		margin-left: 0.5rem;
+		transition: transform 300ms ease-out;
+	}
+
+	&:hover {
+		color: rgba(255, 255, 255, 1) !important;
+		background-color: rgba(255, 0, 255, 0.9);
+		border-color: rgba(255, 0, 255, 0.2);
+		text-decoration: none;
+
+		& .cta-icon {
+			transform: scale(1.15);
+		}
+	}
+
+	@media (min-width: 786px) {
+		width: 50%;
+		margin-left: auto;
+		margin-right: auto;
+	}
+
+	@media (min-width: 991px) {
+		padding: 0.5rem 1rem;
+		font-size: 2rem;
+	}
+
+	@media (min-width: 1280px) {
+		padding: 0.5rem 1rem;
+	}
+`;
+
+class Resume extends Component {
+	componentDidMount() {
+		window.scrollTo(0, 0);
+	}
+
+	render() {
+		return (
+			<Section>
+				<Row>
+					<Col xs="12">
+						<ContentHeader>Only in Select Theaters</ContentHeader>
+					</Col>
+					<Col xs={{ span: '12', order: '3' }} lg={{ span: '10', offset: '1', order: '2' }} xl={{ span: '8', offset: '2' }}>
+						<embed src={Script} type="application/pdf" width="100%" height="793" />
+					</Col>
+					<Col xs={{ span: '12', order: '2' }} lg={{ span: '10', offset: '1', order: '3' }} xl={{ span: '8', offset: '2' }}>
+						<DownloadButton href={Script} download>
+							Download Oist <FontAwesomeIcon className="cta-icon" icon="file-download" />
+						</DownloadButton>
+					</Col>
+				</Row>
+			</Section>
+		);
+	}
+}
+
+export default Resume;
